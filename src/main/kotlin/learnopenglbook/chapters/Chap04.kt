faae127a1355f876ef38ed320dce001198327aea
Angel Biedma <tekeiro@gmail.com>
2020-08-20 15:25:18 +0200

    Fixing chapter 04 of learn open gl book

--- src/main/kotlin/learnopenglbook/chapters/Chap04.kt
+++ src/main/kotlin/learnopenglbook/chapters/Chap04.kt
@@ -43,6 +43,10 @@ object Chap04  {
         glfwDefaultWindowHints() // optional, the current window hints are already the default
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE) // the window will stay hidden after creation
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE) // the window will be resizable
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3)
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3)
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE)
 
         // Create the window
         window = glfwCreateWindow(800, 600, "Hello World!", NULL, NULL)
